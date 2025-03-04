6e44ee68cabd5f5eac42398079fb2a4c0865755a
Bhumi1805 <bhumimulani18@gmail.com>
2025-03-05 00:58:51 +0530

    header

---
+++ src/app/dummy-users.ts
@@ -0,0 +1,32 @@
+export const DUMMY_USERS = [
+  {
+    id: 'u1',
+    name: 'Bhumi Mulani',
+    avatar: 'user-1.jpg',
+  },
+  {
+    id: 'u2',
+    name: 'Mansi Chavan',
+    avatar: 'user-2.jpg',
+  },
+  {
+    id: 'u3',
+    name: 'Shakti Dubey',
+    avatar: 'user-3.jpg',
+  },
+  {
+    id: 'u4',
+    name: 'Hardik Pandya',
+    avatar: 'user-4.jpg',
+  },
+  {
+    id: 'u5',
+    name: 'Khushbu Jain',
+    avatar: 'user-5.jpg',
+  },
+  {
+    id: 'u6',
+    name: 'Abeer Multani',
+    avatar: 'user-6.jpg',
+  },
+];
